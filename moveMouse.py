8617288df71e12fdf034bf84faf512f3728a4a36
Tyler Griggs <tgriggs@college.harvard.edu>
2018-07-26 14:09:17 +0530

    added speed options

--- moveMouse.py
+++ moveMouse.py
@@ -1,6 +1,7 @@
 import win32api
 from win32api import GetSystemMetrics
 
+import sys
 import time
 import math
 import numpy as np
@@ -10,6 +11,17 @@ state_left = win32api.GetKeyState(0x01) # left click
 SCREEN_WIDTH = GetSystemMetrics(0)
 SCREEN_HEIGHT = GetSystemMetrics(1)
 
+
+# set speed from command  line. DEFAULT = .01 seconds
+sleep_time = .01
+if len(sys.argv) > 1:
+	if sys.argv[1] == 'slow':
+		sleep_time = .25
+	elif sys.argv[1] == 'snail':
+		sleep_time = 1
+	elif sys.argv[1] == 'tombrady':
+		sleep_time = 5
+	
 # left click stops the mouse movement
 while(win32api.GetKeyState(0x01) == state_left):
 
@@ -21,7 +33,7 @@ while(win32api.GetKeyState(0x01) == state_left):
     
     win32api.SetCursorPos((x, y))
 
-    time.sleep(.01)
+    time.sleep(sleep_time)
 
     
     
